bd52e44fecf1dc37e25f2c40847077cc8429611a
yanshen1997 <yans@mail.ustc.edu.cn>
2022-06-25 10:18:53 +0800

    init

---
+++ src/kv.rs
@@ -0,0 +1,17 @@
+/// The `KvStore` stores string key/value pairs.
+///
+/// Key/value pairs are stored in a `HashMap` in memory and not persisted to disk.
+///
+/// Example:
+///
+/// ```rust
+/// # use kvs::KvStore;
+/// let mut store = KvStore::new();
+/// store.set("key".to_owned(), "value".to_owned());
+/// let val = store.get("key".to_owned());
+/// assert_eq!(val, Some("value".to_owned()));
+/// ```
+#[derive(Default)]
+pub struct KvStore{
+
+}
