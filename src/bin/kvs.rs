1e3c18fa5ab5d05b3ce1ca95afb8ba2f8a5caa9b
yanshen1997 <yans@mail.ustc.edu.cn>
2022-06-25 15:42:43 +0800

    make test passed.

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,1 +1,85 @@
-fn main() {}
+// Note: this requires the `cargo` feature
+use kvs::KvStore;
+use std::process::exit;
+
+fn main() {
+    let cmd_matches = clap::Command::new("kvs")
+        .bin_name("kvs")
+        .version(env!("CARGO_PKG_VERSION"))
+        .subcommand_required(true)
+        .subcommand(
+            clap::command!("get")
+                .about("Get the string value of a given string key")
+                .arg(
+                    clap::arg!(<KEY>)
+                        .required(true)
+                        .help("A string key")
+                        .value_parser(clap::value_parser!(String)),
+                ),
+        )
+        .subcommand(
+            clap::command!("set")
+                .about("Set the value of a string key to a string")
+                .arg(
+                    clap::arg!(<KEY>)
+                        .required(true)
+                        .help("A string key")
+                        .value_parser(clap::value_parser!(String)),
+                )
+                .arg(
+                    clap::arg!(<VALUE>)
+                        .required(true)
+                        .help("the value of string key")
+                        .value_parser(clap::value_parser!(String)),
+                ),
+        )
+        .subcommand(
+            clap::command!("rm").about("Remove a given key").arg(
+                clap::arg!(<KEY>)
+                    .required(true)
+                    .help("A string key")
+                    .value_parser(clap::value_parser!(String)),
+            ),
+        )
+        .get_matches();
+
+    let mut kv_store = KvStore::new();
+
+    match cmd_matches.subcommand() {
+        Some(("set", _matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Some(("get", _matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Some(("rm", _matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        _ => unreachable!(),
+    }
+    // match cmd_matches.subcommand() {
+    //     Some(("get", sub_matches)) => {
+    //         if let Some(key) = sub_matches.get_one::<String>("KEY").cloned() {
+    //             println!("{}", kv_store.get(key).unwrap());
+    //         }
+    //     }
+    //     Some(("set", sub_matches)) => {
+    //         if let (Some(key), Some(value)) = (
+    //             sub_matches.get_one::<String>("KEY").cloned(),
+    //             sub_matches.get_one::<String>("VALUE").cloned(),
+    //         ) {
+    //             kv_store.set(key, value);
+    //         }
+    //     }
+    //     Some(("remove", sub_matches)) => {
+    //         if let Some(key) = sub_matches.get_one::<String>("KEY").cloned() {
+    //             kv_store.remove(key);
+    //         }
+    //     }
+    //     _ => unreachable!("clap should ensure we don't get here"),
+    // };
+}
+
--- src/kv.rs
+++ src/kv.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 /// The `KvStore` stores string key/value pairs.
 ///
 /// Key/value pairs are stored in a `HashMap` in memory and not persisted to disk.
@@ -12,6 +14,26 @@
 /// assert_eq!(val, Some("value".to_owned()));
 /// ```
 #[derive(Default)]
-pub struct KvStore{
+pub struct KvStore {
+    data: HashMap<String, String>,
+}
 
+impl KvStore {
+    ///
+    pub fn new() -> KvStore {
+        KvStore { data: HashMap::new() }
+    }
+    ///
+    pub fn set(&mut self, key: String, value: String) {
+        self.data.insert(key, value);
+    }
+    ///
+    pub fn get(&self, key: String) -> Option<String> {
+        self.data.get(&key).cloned()
+    }
+
+    ///
+    pub fn remove(&mut self, key: String) {
+        self.data.remove(&key);
+    }
 }
